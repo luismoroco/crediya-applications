c777f55fae3f1753c26643474fcde0e0896d2073
lmorocoramos <lmorocoramos@gmail.com>
2025-08-27 17:06:06 -0500

    chore: remove files.

--- infrastructure/driven-adapters/r2dbc-postgresql/src/test/java/com/crediya/applications/r2dbc/ApplicationReactiveRepositoryAdapterTest.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/test/java/com/crediya/applications/r2dbc/ApplicationReactiveRepositoryAdapterTest.java
@@ -2,18 +2,17 @@ package com.crediya.applications.r2dbc;
 
 import com.crediya.applications.model.application.Application;
 import com.crediya.applications.r2dbc.entity.ApplicationEntity;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.reactivecommons.utils.ObjectMapper;
-import org.springframework.data.domain.Example;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
