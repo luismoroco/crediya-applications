5499f83cb26810cb419839447009b1918376eded
lmorocoramos <lmorocoramos@gmail.com>
2025-09-10 14:22:17 -0500

    feat: add event.

---
+++ domain/model/src/main/java/com/crediya/applications/model/application/gateways/event/ApplicationUpdatedEvent.java
@@ -0,0 +1,35 @@
+package com.crediya.applications.model.application.gateways.event;
+
+import com.crediya.applications.model.application.Application;
+import com.crediya.applications.model.application.ApplicationStatus;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class ApplicationUpdatedEvent {
+
+  private Long applicationId;
+  private Long amount;
+  private Integer deadline;
+  private ApplicationStatus applicationStatus;
+  private String email;
+
+  public static ApplicationUpdatedEvent from(Application application) {
+    return ApplicationUpdatedEvent.builder()
+      .applicationId(application.getApplicationId())
+      .amount(application.getAmount())
+      .deadline(application.getDeadline())
+      .applicationStatus(application.getApplicationStatus())
+      .email(application.getEmail())
+      .build();
+  }
+
+  @Override
+  public String toString() {
+    return String.format("[applicationId=%s][amount=%s][deadline=%s][applicationStatus=%s][email=%s]",
+      applicationId, amount, deadline, applicationStatus, email);
+  }
+}
